3613d580f7341a423515f822f47e2365f66f80bd
Intii <tanapat.samakit@gmail.com>
2021-09-23 06:53:46 +0700

    Add Assignment7

---
+++ Assigned Works/7-Tanapat.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+op = ["[","{","("]
+cp = ["]","}",")"]
+
+def check_Paranthesis(str):
+    Stack = []
+    for i in str:
+        if i in op:
+            Stack.append(i)
+        elif i in cp:
+            cp_pos = cp.index(i)
+            if((len(Stack)>0)) and (op[cp_pos] == Stack[len(Stack)-1]):
+                Stack.pop()
+            else: return "not ok"
+    if len(Stack) == 0: return "ok"
+    
+
+string = "{[]{(Test)}}"
+print(string,"-", check_Paranthesis(string))
+string = "[{}{})(Test]"
+print(string,"-", check_Paranthesis(string))
+string = "(Test) Hello"
+print(string,"-", check_Paranthesis(string))
+string = "(Test) Hello(())"
+print(string,"-", check_Paranthesis(string))
+
+
+# In[ ]:
+
+
+
+
